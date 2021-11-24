1a4856691975a15cc4882abcf593ec7d71c2600d
armaan <armaan@gmail.com>
2021-11-25 00:39:29 +0600

    design added

--- resources/views/admin/sections/dashboard.blade.php
+++ resources/views/admin/sections/dashboard.blade.php
@@ -1,9 +1,95 @@
 @extends('admin.layouts.app')
 
 @section('main-content')
+<div class="row">
+  <div class="col-md-12">
+   <div class="row">
+     <div class="col-md-3"></div>
+     <div class="col-md-6">
+        <div class="h2 text-center m-4"><span class="badge badge-primary text-white text-bold">Customize Blocks</span></div>
+     </div>
+     <div class="col-md-3"></div>
+   </div>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="row">
+          <div class="col-md-3">
+               <div class="card">
+                 <div class="card-header text-center bg-dark text-white text-bold" style="font-size: 14px;">About Me</div>
+                 <div class="card-body bg-white">
+                   <span class="badge badge-info"><a href="{{ route('aboutme') }}" target="" class="text-white">Customize</a></span>
+                 </div>
+               </div>
+            </div> <div class="col-md-3">
+               <div class="card">
+                 <div class="card-header text-center bg-dark text-white text-bold">skill</div>
+                 <div class="card-body bg-white">
+                   <span class="badge badge-info"><a href="{{ route('skill') }}" target="" class="text-white">Customize</a></span>
+                 </div>
+               </div>
+            </div> <div class="col-md-3">
+               <div class="card">
+                 <div class="card-header text-center bg-dark text-white text-bold">Service</div>
+                 <div class="card-body bg-white">
+                   <span class="badge badge-info"><a href="{{ route('service') }}" target="" class="text-white">Customize</a></span>
+                 </div>
+               </div>
+            </div> <div class="col-md-3">
+               <div class="card">
+                 <div class="card-header text-center bg-dark text-white text-bold">Work</div>
+                 <div class="card-body bg-white">
+                   <span class="badge badge-info"><a href="{{ route('work') }}" target="" class="text-white">Customize</a></span>
+                 </div>
+               </div>
+            </div>
+        </div>
+      
+    </div>
+
+     <div class="col-md-6">
+        <div class="row">
+          <div class="col-md-3">
+               <div class="card">
+                 <div class="card-header text-center bg-dark text-white text-bold">Header</div>
+                 <div class="card-body bg-white">
+                   <span class="badge badge-info"><a href="{{ route('homeheader') }}" target="" class="text-white">Customize</a></span>
+                 </div>
+               </div>
+            </div> <div class="col-md-3">
+               <div class="card">
+                 <div class="card-header text-center bg-dark text-white text-bold">Slider</div>
+                 <div class="card-body bg-white">
+                   <span class="badge badge-info"><a href="{{ route('slider') }}"  target="" class="text-white">Customize</a></span>
+                 </div>
+               </div>
+            </div> <div class="col-md-3">
+               <div class="card">
+                 <div class="card-header text-center bg-dark text-white text-bold">Blog</div>
+                 <div class="card-body bg-white">
+                   <span class="badge badge-info"><a href="{{ route('blog') }}" target="" class="text-white">Customize</a></span>
+                 </div>
+               </div>
+            </div> <div class="col-md-3">
+               <div class="card">
+                 <div class="card-header text-center bg-dark text-white text-bold">Review</div>
+                 <div class="card-body bg-white">
+                   <span class="badge badge-info"><a href="{{ route('review') }}"  target="" class="text-white">Customize</a></span>
+                 </div>
+               </div>
+            </div>
+        </div>
+      </div>
+  </div>
 
 <div class="row">
 	<div class="col-md-12">
+    <div class="row">
+     <div class="col-md-3"></div>
+     <div class="col-md-6">
+        <div class="h2 text-center m-4"><span class="badge badge-info text-white text-bold">All Messages</span></div>
+     </div>
+     <div class="col-md-3"></div>
+   </div>
 		<table class="table table-striped">
   <thead>
     <tr>
@@ -32,6 +118,13 @@
   </tbody>
 </table>
 	</div>
+</div>
+
+
+
+ 
+   
+ 
 </div>
  
 @endsection
